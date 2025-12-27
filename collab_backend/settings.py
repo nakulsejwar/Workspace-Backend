e6a1646c0043e87fdc7c477ec295b44b65e2c676
nakulsejwar <nakulsejwar02@gmail.com>
2025-12-27 20:19:00 +0530

    deploy

--- collab_backend/settings.py
+++ collab_backend/settings.py
@@ -13,14 +13,19 @@ SECRET_KEY = 'django-insecure-ltx$@bkqy46vl87$k(zi&k7-j(4*cb45+aph-4hk!o2b@%+^1a
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "1") == "1"
+DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".railway.app",
+]
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://127.0.0.1:8000",
-    "http://localhost:8000",
+    "https://*.railway.app",
 ]
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 
 
